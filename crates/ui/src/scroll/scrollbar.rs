WswDay2022/HyperGPUI

commit 80432b4b623e353bfaff4584e7d68db86718e634

tab_bar: Fix some incorrect style in TabBar. (#1078)
This change to fix some incorrect style use, that will break in Taffy
0.5.2+

Ref https://github.com/zed-industries/zed/pull/34827

- Add `Tab::empty` to create a Tab without label.
- Fix form layout in Taffy new version.
- Update GPUI for scroll API changes.

crates/ui/src/scroll/scrollbar.rs
@@ -69,7 +69,7 @@ impl ScrollHandleOffsetable for ScrollHandle {
     }
 
     fn content_size(&self) -> Size<Pixels> {
-        self.padded_content_size()
+        self.max_offset()
     }
 }
 
@@ -87,7 +87,7 @@ impl ScrollHandleOffsetable for UniformListScrollHandle {
     }
 
     fn content_size(&self) -> Size<Pixels> {
-        self.0.borrow().base_handle.padded_content_size()
+        self.0.borrow().base_handle.max_offset()
     }
 }
 

Cargo.toml
@@ -15,6 +15,7 @@ resolver = "2"
 
 [workspace.dependencies]
 gpui = { git = "https://github.com/zed-industries/zed.git" }
+reqwest_client = { git = "https://github.com/zed-industries/zed.git" }
 gpui-component = { path = "crates/ui" }
 gpui-component-macros = { path = "crates/macros" }
 story = { path = "crates/story" }

crates/story/Cargo.toml
@@ -14,7 +14,7 @@ fake = { version = "2.10.0", features = ["dummy"] }
 rand = "0.8"
 raw-window-handle = { version = "0.6", features = ["std"] }
 regex = "1"
-reqwest_client = { git = "https://github.com/zed-industries/zed.git" }
+reqwest_client.workspace = true
 rust-embed = "8.5.0"
 serde = "1"
 serde_json = "1"

crates/ui/src/dock/tab_panel.rs
@@ -696,7 +696,7 @@ impl TabPanel {
                 }
 
                 Some(
-                    Tab::new("")
+                    Tab::empty()
                         .map(|this| {
                             if let Some(tab_name) = panel.tab_name(cx) {
                                 this.child(tab_name)
@@ -704,7 +704,6 @@ impl TabPanel {
                                 this.child(panel.title(window, cx))
                             }
                         })
-                        .py_2()
                         .selected(active)
                         .on_click(cx.listener({
                             let is_collapsed = self.collapsed;

crates/ui/src/form.rs
@@ -394,7 +394,13 @@ impl RenderOnce for FormField {
                                 }),
                         )
                     })
-                    .child(div().flex_1().overflow_x_hidden().child(self.child)),
+                    .child(
+                        div()
+                            .w_full()
+                            .flex_1()
+                            .overflow_x_hidden()
+                            .child(self.child),
+                    ),
             )
             .child(
                 // Other

crates/ui/src/tab/tab.rs
@@ -379,7 +379,7 @@ impl TabVariant {
 pub struct Tab {
     id: ElementId,
     base: Div,
-    pub(super) label: SharedString,
+    pub(super) label: Option<SharedString>,
     icon: Option<Icon>,
     prefix: Option<AnyElement>,
     suffix: Option<AnyElement>,
@@ -427,8 +427,8 @@ impl Default for Tab {
     fn default() -> Self {
         Self {
             id: ElementId::Integer(0),
-            base: div().gap_1(),
-            label: SharedString::new(""),
+            base: div(),
+            label: None,
             icon: None,
             children: Vec::new(),
             disabled: false,
@@ -446,8 +446,13 @@ impl Tab {
     /// Create a new tab with a label.
     pub fn new(label: impl Into<SharedString>) -> Self {
         let mut this = Self::default();
-        this.label = label.into();
+        this.label = Some(label.into());
         this
+    }
+
+    /// Create an empty tab.
+    pub fn empty() -> Self {
+        Self::default()
     }
 
     /// Create a Icon tab.
@@ -579,12 +584,12 @@ impl RenderOnce for Tab {
         let inner_margins = self.variant.inner_margins(self.size);
         let inner_height = self.variant.inner_height(self.size);
         let height = self.variant.height(self.size);
-        let has_label = !self.label.is_empty();
 
         self.base
             .id(self.id)
             .flex()
             .flex_wrap()
+            .gap_1()
             .items_center()
             .flex_shrink_0()
             .overflow_hidden()
@@ -637,8 +642,11 @@ impl RenderOnce for Tab {
                         }
                         None => this
                             .paddings(inner_paddings)
-                            .when(has_label, |this| this.child(self.label))
-                            .when(!has_label, |this| this.children(self.children)),
+                            .map(|this| match self.label {
+                                Some(label) => this.child(label),
+                                None => this,
+                            })
+                            .children(self.children),
                     })
                     .bg(tab_style.inner_bg)
                     .rounded(tab_style.inner_radius)

crates/ui/src/tab/tab_bar.rs
@@ -280,7 +280,7 @@ impl RenderOnce for TabBar {
                             this = this.scrollable();
                             for (ix, (label, disabled)) in item_labels.iter().enumerate() {
                                 this = this.menu_with_check_and_disabled(
-                                    label.clone(),
+                                    label.clone().unwrap_or_default(),
                                     selected_index == Some(ix),
                                     Box::new(SelectTab(ix)),
                                     *disabled,

commit 66cb8532a84acba6429d82bd0fd8f08feed4b4bb

scrollbar: Fix content size (#1086)
Caused by PR #1078 incorrect changes, and GPUI was changed API
https://github.com/zed-industries/zed/pull/34832

The content size of the scrollable area should be its max scroll offset
plus the container size.

---------

Co-authored-by: Jason Lee <huacnlee@gmail.com>

crates/ui/src/scroll/scrollbar.rs
@@ -69,7 +69,7 @@ impl ScrollHandleOffsetable for ScrollHandle {
     }
 
     fn content_size(&self) -> Size<Pixels> {
-        self.max_offset()
+        self.max_offset() + self.bounds().size
     }
 }
 
@@ -87,7 +87,8 @@ impl ScrollHandleOffsetable for UniformListScrollHandle {
     }
 
     fn content_size(&self) -> Size<Pixels> {
-        self.0.borrow().base_handle.max_offset()
+        let base_handle = &self.0.borrow().base_handle;
+        base_handle.max_offset() + base_handle.bounds().size
     }
 }
 