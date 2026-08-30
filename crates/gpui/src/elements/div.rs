d6d6c144d4972ddf897be7a21864be5eb99b8844
Dustin Yost <dustin.yost.t@gmail.com>
2026-08-30 00:23:55 -0400

    reduce constraints on Stateful element trait impls (#128)

--- crates/gpui/src/elements/div.rs
+++ crates/gpui/src/elements/div.rs
@@ -3982,12 +3982,7 @@ where
     }
 }
 
-impl<E> StatefulInteractiveElement for Stateful<E>
-where
-    E: Element,
-    Self: InteractiveElement,
-{
-}
+impl<E> StatefulInteractiveElement for Stateful<E> where Self: InteractiveElement {}
 
 impl<E> InteractiveElement for Stateful<E>
 where
@@ -4074,14 +4069,11 @@ where
     }
 }
 
-impl<E> IntoElement for Stateful<E>
-where
-    E: Element,
-{
-    type Element = Self;
+impl<E: IntoElement> IntoElement for Stateful<E> {
+    type Element = E::Element;
 
     fn into_element(self) -> Self::Element {
-        self
+        self.element.into_element()
     }
 }
 
